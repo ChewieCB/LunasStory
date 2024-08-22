53b0dcb19fb4ee851a8a409d36ba835eb2f683f7
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-08-22 21:58:48 +0100

    Added tool assets

--- src/entities/hand_cursor/hitbox_component.gd
+++ src/entities/hand_cursor/hitbox_component.gd
@@ -1,13 +1,2 @@
 extends BaseComponent
 class_name HitboxComponent
-
-signal body_entered
-signal body_exited
-
-
-func _on_body_entered(body: Node2D) -> void:
-	emit_signal("body_entered")
-
-
-func _on_body_exited(body: Node2D) -> void:
-	emit_signal("body_exited")
