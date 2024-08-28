59e9c2d898878986344a320cf24696d59823e78f
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-08-28 16:07:06 +0100

    Moved bulky signal connections into methods in shared class

--- src/components/Selectable/selectable_component.gd
+++ src/components/Selectable/selectable_component.gd
@@ -1,9 +1,11 @@
 extends BaseComponent
 class_name SelectableComponent
 
-signal hover(state: bool)
+signal hover(entity: Node2D, state: bool)
 
 @export var sprite: Sprite2D
+
+@onready var entity := get_parent()
 
 
 func _ready():
@@ -12,9 +14,9 @@ func _ready():
 
 func _on_area_entered(area: Area2D) -> void:
 	if is_enabled():
-		emit_signal("hover", true)
+		emit_signal("hover", entity, true)
 
 
 func _on_area_exited(area: Area2D) -> void:
 	if is_enabled():
-		emit_signal("hover", false)
+		emit_signal("hover", entity, false)
