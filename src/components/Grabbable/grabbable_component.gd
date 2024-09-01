20c6cc572cfb84d15f4306de57e3623fba1e03d3
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-09-01 14:01:01 +0100

    Added state chart to GrabbableComponent to better isolate grab inputs

--- src/components/Grabbable/grabbable_component.gd
+++ src/components/Grabbable/grabbable_component.gd
@@ -5,11 +5,14 @@ signal pickup(entity: Node2D)
 signal drop(entity: Node2D)
 
 @export var selectable_component: SelectableComponent
+@export var collider: CollisionShape2D
+@export var state_chart: StateChart
 @onready var entity := get_parent()
 
 
 func _ready() -> void:
 	selectable_component.hover.connect(_on_hover)
+	self.input_event.connect(_on_input_event)
 	add_to_group("grabbable")
 	disable()
 
@@ -17,14 +20,23 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if is_enabled():
 		if Input.is_action_just_released("interact"):
-			emit_signal("drop", entity)
+			state_chart.send_event("drop")
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if is_enabled():
 		if Input.is_action_just_pressed("interact"):
-			emit_signal("pickup", entity)
+			state_chart.send_event("pickup")
 
 
 func _on_hover(_entity: Node2D, state: bool) -> void:
 	enable() if state == true else disable()
+
+
+func _on_held_state_entered() -> void:
+	emit_signal("pickup", entity)
+
+
+func _on_dropped_state_entered() -> void:
+	emit_signal("drop", entity)
+	state_chart.send_event("settle")
