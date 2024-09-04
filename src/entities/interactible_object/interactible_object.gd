ce441748b1aca8028c6d95f8619616019da778fa
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-09-04 21:50:39 +0100

    InteractableObjects disable components on death and change texture

--- src/entities/interactible_object/interactible_object.gd
+++ src/entities/interactible_object/interactible_object.gd
@@ -15,6 +15,7 @@ signal item(_item: Node2D, state: bool)
 @export var sprite: Sprite2D
 @export var object_texture: Texture
 @export var hover_texture: Texture
+@export var disabled_texture: Texture
 @export var randomize_texture: bool = false
 @export var texture_index: int = 1
 
@@ -30,10 +31,14 @@ func _ready() -> void:
 		follow_component.target = follow_target
 	
 	selectable_component.hover.connect(_on_hover)
+	selectable_component.was_disabled.connect(toggle_hover_texture.bind(false))
 	
 	if grabbable_component:
 		grabbable_component.pickup.connect(_on_pickup)
 		grabbable_component.drop.connect(_on_drop)
+	
+	if health_component:
+		health_component.died.connect(_on_died)
 
 
 func toggle_hover_texture(state: bool):
@@ -69,3 +74,13 @@ func _on_drop(entity: Node2D) -> void:
 		# Re-enables grabbing if the cursor is still over the object
 		selectable_component.query_hover()
 		_handle_item(false)
+
+
+func _on_died() -> void:
+	selectable_component.disable()
+	grabbable_component.disable()
+	follow_component.disable()
+	hitbox_component.disable()
+	
+	if disabled_texture:
+		sprite.texture = disabled_texture
