76c512f95ffadfaf6afce822063a289a1a48a2f7
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-09-15 21:50:26 +0100

    Added main player health UI

--- src/ui/health_bar/health_bar.gd
+++ src/ui/health_bar/health_bar.gd
@@ -3,13 +3,17 @@ extends TextureProgressBar
 @export_category("Components")
 @export var health_component: HealthComponent
 
+@export var hide_ui_on_death: bool = false
+
 @onready var timer: Timer = $Timer
 @onready var damage_bar: TextureProgressBar = $DamageBar
 
 
 func _ready() -> void:
+	await get_owner().ready
 	init_health_ui(health_component.current_health)
 	health_component.health_changed.connect(_on_health_changed)
+	health_component.died.connect(_on_died)
 
 
 func init_health_ui(_health) -> void:
@@ -26,6 +30,12 @@ func _on_health_changed(new_health: float, prev_health: float) -> void:
 		timer.start()
 	else:
 		damage_bar.value = new_health
+
+
+func _on_died() -> void:
+	if hide_ui_on_death:
+		self.visible = false
+	# TODO - animations
 
 
 func _on_timer_timeout():
