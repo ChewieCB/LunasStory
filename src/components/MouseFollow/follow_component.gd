dd89460edc8e8c66f7259deba4d2d963aa77b9e0
ChewieCB <jackmccaffrey96@gmail.com>
2024-09-30 08:52:33 +0100

    WIP - grid locked movement

--- src/components/MouseFollow/follow_component.gd
+++ src/components/MouseFollow/follow_component.gd
@@ -4,7 +4,16 @@ class_name FollowComponent
 @onready var entity: Node2D = get_parent()
 
 @export var target: Node2D
-var follow_global_position: Vector2
+var follow_global_position: Vector2:
+	set(value):
+		follow_global_position = value
+		if lock_to_grid:
+			move_entity_within_grid(follow_global_position)
+		else:
+			entity.global_position = follow_global_position
+
+@export var lock_to_grid: bool = false
+@export var tilemap: TileMapLayer
 
 
 func _ready():
@@ -13,7 +22,7 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	if is_enabled():
-		entity.global_position = get_target_global_position()
+		follow_global_position = get_target_global_position()
 	else:
 		# TODO - lerp towards nearest valid position
 		pass
@@ -23,3 +32,45 @@ func get_target_global_position() -> Vector2:
 	if target:
 		return target.get_current_cursor_marker().global_position
 	return entity.global_position
+
+
+func move_entity_within_grid(pos: Vector2) -> void:
+	if not tilemap:
+		push_error(
+			"Lock to grid enabled but no tilemap is assigned to %s.%s" % [
+				owner.name, self.name
+			]
+		)
+		return
+	
+	var local_pos = entity.to_local(pos)
+	var cell_coords = tilemap.local_to_map(local_pos)
+	var cell_local_pos = tilemap.map_to_local(cell_coords)
+	var cell_global_pos = entity.to_global(cell_local_pos)
+	
+	# Only allow placement on valid floor cells
+	var cell_data: TileData = tilemap.get_cell_tile_data(cell_coords)
+	if cell_data:
+		# 0: Walls, 1: Floor
+		var cell_type = cell_data.terrain
+		match cell_type:
+			0:
+				print_rich(
+					"cell_type: %s" % ["[color=yellow]Wall[/color]"]
+				)
+			1:
+				print_rich(
+					"cell_type: %s" % ["[color=green]Floor[/color]"]
+				)
+			_:
+				print_rich(
+					"cell_type: %s" % ["[color=red]Invalid[/color]"]
+				)
+	
+	# Debug cell visualisation
+	entity.local_pos_DEBUG = local_pos
+	entity.cell_local_pos_DEBUG = cell_local_pos
+	entity.cell_global_pos_DEBUG = cell_global_pos
+	entity.cell_rect_DEBUG = Rect2(cell_global_pos, tilemap.tile_set.tile_size)
+	
+	entity.global_position = cell_global_pos + Vector2(tilemap.tile_set.tile_size / 2)
--- src/entities/furniture/furniture_big.gd
+++ src/entities/furniture/furniture_big.gd
@@ -1,16 +1,36 @@
 extends InteractibleObject
+# TODO - extend new class for PlaceableObjects that update navs
 class_name FurnitureBig
 
 @export_category("Components")
 @export var dynamic_nav_obstacle: DynamicNavObstacleComponent
 @export var particles_component: ParticlesComponent
 
-#@export var consumption_particle: ParticleResource
+@export var follow_component_tilemap: TileMapLayer:
+	set(value):
+		follow_component_tilemap = value
+		follow_component.tilemap = follow_component_tilemap
+
+var local_pos_DEBUG: Vector2
+var cell_local_pos_DEBUG: Vector2
+var cell_global_pos_DEBUG: Vector2
+var cell_rect_DEBUG: Rect2
 
 
 func _ready() -> void:
 	super()
 	add_to_group("furniture_big")
+
+
+func _draw() -> void:
+	draw_circle(local_pos_DEBUG, 1.0, Color.ORANGE)
+	draw_circle(cell_local_pos_DEBUG, 1.0, Color.YELLOW)
+	draw_circle(cell_global_pos_DEBUG, 1.0, Color.GREEN)
+	draw_rect(cell_rect_DEBUG, Color(Color.RED), 0.5)
+
+
+func _process(_delta: float) -> void:
+	queue_redraw()
 
 
 func _on_pickup(entity: Node2D) -> void:
