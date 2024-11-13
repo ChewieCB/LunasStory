1dc9256cdba1b7f1ff088b8ffe00bdbe1047d6c4
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-11-13 23:16:23 +0000

    Added function hooks for generating recipe UI

--- src/ui/potion_book/ingredient_ui.gd
+++ src/ui/potion_book/ingredient_ui.gd
@@ -2,3 +2,9 @@ extends MarginContainer
 
 @onready var ingredient_texture: TextureRect = $HBoxContainer/MarginContainer/TextureRect
 @onready var ingredient_count_label: RichTextLabel = $HBoxContainer/MarginContainer2/RichTextLabel
+
+
+func set_label(current: int = 0, max: int = 0) -> String:
+	var label_str = "[center]%s/%s[/center]" % [current, max]
+	ingredient_count_label.text = label_str
+	return label_str
--- src/ui/potion_book/potion_book_ui.gd
+++ src/ui/potion_book/potion_book_ui.gd
@@ -1,22 +1,30 @@
 extends Control
 
-@onready var health_bar: TextureProgressBar = $PortraitMargin/HealthBar
 @onready var portrait: TextureRect = $PortraitMargin/Portrait
+@onready var ingredient_ui_container: VBoxContainer = $MarginContainer/TextureRect/MarginContainer/HBoxContainer/IngredientContainer
 
 
-func _ready() -> void:
-	if health_component:
-		health_component.health_changed.connect(_on_health_changed)
-		health_bar.health_component = health_component
-		portrait.texture = damage_portrait_texures[0]
+func set_potion_recipe(recipe: PotionRecipe) -> void:
+	clear_ingredients()
+	portrait.texture = recipe.potion.icon
+	for i in range(recipe.ingredient_counts.size()):
+		var ic = recipe.ingredient_counts[i]
+		set_ingredient_ui(ic.ingredient.icon, 0, ic.count, i)
 
 
-func _on_health_changed(new_health: float, prev_health: float) -> void:
-	if new_health > 0:
-		var portrait_idx: int = remap(
-			new_health, 
-			health_component.max_health, 0,
-			0, damage_portrait_texures.size()
-		)
-		portrait.texture = damage_portrait_texures[portrait_idx]
-		print("Portrait idx = %s at %s health" % [portrait_idx, new_health])
+func set_ingredient_ui(icon: Texture2D, current_count: int = 0, max_count: int = 0, index: int = 0) -> void:
+	if index >= 3:
+		push_error("Ingredient UI index exceeds maximum index.")
+		return
+	
+	var ui = ingredient_ui_container.get_child(index)
+	ui.ingredient_texture = icon
+	ui.set_label(current_count, max_count)
+	ui.visible = true
+
+
+func clear_ingredients() -> void:
+	for ui_node in ingredient_ui_container.get_children():
+		ui_node.visible = false
+		ui_node.set_label()
+		ui_node.ingredient_texture = null
